20e218b662324c1e0e90488e3e0542b7c73594e6
titov-andrei <titov_andrei@mail.ru>
2020-10-22 21:48:49 +0300

    Update widget.dart

--- lib/widgets/widget.dart
+++ lib/widgets/widget.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 Widget BrandName() {
   return Row(
     children: <Widget>[
-      
+      Text("Wallpaper", style: TextStyle(color: Colors.black87),),
+      Text("Hub", style: TextStyle(color: Colors.blue),)
     ],
   );
 }
