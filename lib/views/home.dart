5da026dc9294e70b9709a2aed33a8bdd5111f1d1
titov-andrei <titov_andrei@mail.ru>
2020-10-22 21:48:42 +0300

    Update home.dart

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wallpaperhubapp/widgets/widget.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -10,7 +11,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
+        title: BrandName(),
       ),
     );
   }
