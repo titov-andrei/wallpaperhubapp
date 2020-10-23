2074ca60e3b3d116fb230203d35d49def45f6ea0
titov-andrei <titov_andrei@mail.ru>
2020-10-23 09:18:59 +0300

    Update widget.dart

--- lib/widgets/widget.dart
+++ lib/widgets/widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 Widget BrandName() {
   return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
     children: <Widget>[
       Text("Wallpaper", style: TextStyle(color: Colors.black87),),
       Text("Hub", style: TextStyle(color: Colors.blue),)
