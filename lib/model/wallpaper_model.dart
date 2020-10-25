6f57374e05a7ba3e4df75621e2b9425767b6fc29
titov-andrei <titov_andrei@mail.ru>
2020-10-25 16:29:21 +0300

    Update wallpaper_model.dart

--- lib/model/wallpaper_model.dart
+++ lib/model/wallpaper_model.dart
@@ -12,11 +12,10 @@ class WallpaperModel {
 
   factory WallpaperModel.fromMap(Map<String, dynamic> jsonData) {
     return WallpaperModel(
-      src: SrcModel.fromMap(jsonData["src"]),
-      photographer_url: jsonData["photographer_url"],
-      photographer_id: jsonData["photographer_id"],
-      photographer: jsonData["photographer"]
-    );
+        src: SrcModel.fromMap(jsonData["src"]),
+        photographer_url: jsonData["photographer_url"],
+        photographer_id: jsonData["photographer_id"],
+        photographer: jsonData["photographer"]);
   }
 }
 
@@ -27,7 +26,11 @@ class SrcModel {
 
   SrcModel({this.portrait, this.original, this.small});
 
-  factory SrcModel.fromMap(Map<String, dynamic> jsonData){
-    return SrcModel
+  factory SrcModel.fromMap(Map<String, dynamic> jsonData) {
+    return SrcModel(
+      portrait: jsonData["portrait"],
+      original: jsonData["original"],
+      small: jsonData["small"],
+    );
   }
 }
