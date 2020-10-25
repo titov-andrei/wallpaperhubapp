0043e2ce34f5e55dd22f479afbf24623b00015eb
titov-andrei <titov_andrei@mail.ru>
2020-10-25 18:07:20 +0300

    Update widget.dart

--- lib/widgets/widget.dart
+++ lib/widgets/widget.dart
@@ -4,14 +4,27 @@ Widget brandName() {
   return Row(
     mainAxisAlignment: MainAxisAlignment.center,
     children: <Widget>[
-      Text("Wallpaper", style: TextStyle(color: Colors.black87),),
-      Text("Hub", style: TextStyle(color: Colors.blue),)
+      Text(
+        "Wallpaper",
+        style: TextStyle(color: Colors.black87),
+      ),
+      Text(
+        "Hub",
+        style: TextStyle(color: Colors.blue),
+      )
     ],
   );
 }
 
-Widget WallpapersList(List<WallpaperModel> wallpapers){
+Widget WallpapersList(List<WallpaperModel> wallpapers, context) {
   return Container(
-    child: GridView.count(crossAxisCount: null),
-  )
+    padding: EdgeInsets.symmetric(horizontal: 16),
+    child: GridView.count(
+      crossAxisCount: 2,
+      childAspectRatio: 0.6,
+      mainAxisSpacing: 6.0,
+      crossAxisSpacing: 6.0,
+      children: <Widget>[],
+    ),
+  );
 }
