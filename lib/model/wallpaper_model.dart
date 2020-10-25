1511f636904daf0004a95ebf1f818d1b8bbef1ea
titov-andrei <titov_andrei@mail.ru>
2020-10-25 12:25:32 +0300

    Update wallpaper_model.dart

--- lib/model/wallpaper_model.dart
+++ lib/model/wallpaper_model.dart
@@ -1,12 +1,20 @@
 class WallpaperModel {
   String photographer;
   String photographer_url;
-  String photographer_id;
+  int photographer_id;
   SrcModel src;
+
+  WallpaperModel(
+      {this.src,
+      this.photographer_url,
+      this.photographer_id,
+      this.photographer});
 }
 
 class SrcModel {
   String original;
   String small;
   String portrait;
+
+  SrcModel({this.portrait, this.original, this.small});
 }
